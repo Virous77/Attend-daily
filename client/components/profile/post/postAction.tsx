5bc55a877c690ffdf97f33f8f37bdcd2e1de0ed6
Reetesh Kumar <rajreetesh7@gmail.com>
2023-10-30 23:41:28 +0530

    added icons for post action

--- client/components/profile/post/postAction.tsx
+++ client/components/profile/post/postAction.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { PostListProps } from "./postList";
+import { AiOutlineHeart, AiTwotoneHeart } from "react-icons/ai";
+import { FaRegComment, FaRetweet } from "react-icons/fa";
+import { BsBookmarkCheckFill, BsBookmarkCheck } from "react-icons/bs";
 
 const PostAction: React.FC<PostListProps> = ({ post }) => {
   return <div>PostAction</div>;
