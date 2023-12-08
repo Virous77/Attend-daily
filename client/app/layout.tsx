8dfb6937cd5b3ef41638aa4a64cf344a8a1825c6
Reetesh Kumar <rajreetesh7@gmail.com>
2023-12-08 12:54:51 +0530

    updated theme

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -54,12 +54,9 @@ export default async function RootLayout({
           content="width=device-width; initial-scale=1; viewport-fit=cover; maximum-scale=1; user-scalable=no"
         />
         <meta name="robots" content="index, follow" />
-        <meta name="mobile-web-app-capable" content="yes" />
+        {/* <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" /> */}
       </head>
       <body className={roboto.className}>
         <ThemeProvider
